73620199d4ffd1e5b562949aa1707b8e7854aca4
shounak00 <shounak00@gmail.com>
2020-08-23 03:09:29 +0600

    Basic scripts

---
+++ Ex4/Ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ex4
+{
+	/// <summary>
+	/// Exercise 4 Solution
+	/// </summary>
+	class MainClass
+	{
+
+		public static void Main(string[] args)
+		{
+			// get locations
+			Console.Write("Enter altitude at first location:  ");
+			int firstAltitude = int.Parse(Console.ReadLine());
+			Console.Write("Enter altitude at second location: ");
+			int secondAltitude = int.Parse(Console.ReadLine());
+
+			// calculate and print altitude change
+			int altitudeChange = secondAltitude - firstAltitude;
+			Console.WriteLine();
+			Console.WriteLine("Altitude change: " + altitudeChange);
+
+			Console.WriteLine();
+		}
+	}
+}
---
+++ IntegerDataTypes/IntegerDataTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IntegerDataTypes
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int totalSecondsPlayed = 100;
+			int minutesPlayed = totalSecondsPlayed/60; 
+
+			Console.WriteLine ("Minutes Played "+ minutesPlayed);
+		}
+	}
+}
