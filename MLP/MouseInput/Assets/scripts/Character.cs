268e977680c8406c6a32c5895b59eb3cdd2590b8
shounak00 <shounak00@gmail.com>
2020-08-24 07:39:09 +0600

    Input codes

---
+++ MLP/MouseInput/Assets/scripts/Character.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A character
+/// </summary>
+public class Character : MonoBehaviour
+{
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+		
+	}
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+    {
+        // convert mouse position to world position
+        Vector3 position = Input.mousePosition;
+        position.z = -Camera.main.transform.position.z;
+        position = Camera.main.ScreenToWorldPoint(position);
+
+        // move to mouse position
+        transform.position = position;
+    }
+}
